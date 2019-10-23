4472ec9bb6e1463da3286e669ba310c30780fe9c
Olei Eto <teneeto@gmail.com>
2019-10-23 04:40:35 +0100

    can find many one item

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,37 +1,10 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 const debug = require('debug')('app:bookRoutes');
 
 const bookRouter = express.Router();
 
 function router(nav) {
-  // const books = [
-  //   {
-  //     title: 'War and Peace',
-  //     genre: 'historical Fiction',
-  //     author: 'Lev Nikolayev',
-  //     read: false,
-  //   },
-  //   {
-  //     title: 'Les Misérables',
-  //     genre: 'historical Fiction',
-  //     author: 'Victor Hugo',
-  //     read: false,
-  //   },
-  //   {
-  //     title: 'Things Fall Apart',
-  //     genre: 'Fiction',
-  //     author: 'Chinwa Achebe',
-  //     read: false,
-  //   },
-  //   {
-  //     title: 'Homlet',
-  //     genre: 'Novel',
-  //     author: 'Williams Shakespere',
-  //     read: false,
-  //   },
-  // ];
-
   bookRouter.route('/')
     .get((req, res) => {
       const url = 'mongodb://localhost:27017';
@@ -61,11 +34,30 @@ function router(nav) {
   bookRouter.route('/:id')
     .get((req, res) => {
       const { id } = req.params;
-      res.render('book', {
-        nav,
-        title: 'Library',
-        book: books[id],
-      });
+
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url, { useUnifiedTopology: true });
+          debug('connected successfully to server');
+          const db = client.db(dbName);
+          const col = await db.collection('books');
+          const book = await col.findOne({ _id: new ObjectID(id) });
+          debug(book);
+          res.render('book', {
+            nav,
+            title: 'Library',
+            book,
+          });
+        } catch (err) {
+          debug(err.stack);
+        }
+      }());
+
+
     });
 
   return bookRouter;
